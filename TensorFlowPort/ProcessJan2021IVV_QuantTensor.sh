ae89746cc5e53bd2119e61eeee38597cd4fb3ab2
Enoch Yeung <enochyeung@Enochs-MacBook-Pro.local>
2021-01-14 11:18:03 -0800

    latest updates

--- TensorFlowPort/ProcessJan2021IVV_QuantTensor.sh
+++ TensorFlowPort/ProcessJan2021IVV_QuantTensor.sh
@@ -2,13 +2,13 @@
 
 #python3 DynamicFeatureAnalytics.py "/FoFData/20201012 IV_V A-549 P1/20201012 IV_V A-549 P1" >> AP1.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20210112 IVV A549 Plate01/210112_163221_Plate 1" >> Jan21IVV_A549Plate1.txt &
+python3 DynamicFeatureAnalytics.py "/FoFData/20210112 IVV A549 Plate01/210112_163221_Plate 1" >> Jan21IVV_A549Plate1.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20210112 IVV A549 Plate02/210112_164130_Plate 1" >> Jan21IVV_A549Plate2.txt &
+python3 DynamicFeatureAnalytics.py "/FoFData/20210112 IVV A549 Plate02/210112_164130_Plate 1" >> Jan21IVV_A549Plate2.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20210112 IVV A549 Plate03/210112_165035_Plate 1" >> Jan21IVV_A549Plate3.txt &
+python3 DynamicFeatureAnalytics.py "/FoFData/20210112 IVV A549 Plate03/210112_165035_Plate 1" >> Jan21IVV_A549Plate3.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20210112 IVV A549 Plate04/210112_165944_Plate 1"  >> Jan21IVV_A549Plate4.txt &
+python3 DynamicFeatureAnalytics.py "/FoFData/20210112 IVV A549 Plate04/210112_165944_Plate 1"  >> Jan21IVV_A549Plate4.txt &
 
 
 

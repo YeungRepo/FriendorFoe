c05d0aeb9656a8164c1f94dbcdb23c5b3396173c
Enoch Yeung <enochyeung@Enochs-MacBook-Pro.local>
2021-01-13 19:34:40 -0800

    latest updates

--- TensorFlowPort/DynamicFeatureAnalytics_HCT116.py
+++ TensorFlowPort/DynamicFeatureAnalytics_HCT116.py
@@ -42,7 +42,7 @@ im = Image.open("../Data/Kernels/HCT116-Healthy_NC_10_2_20.tif");
 #print(im_cv)
 #imshow(np.asarray(im))
 im_array_gray = np.asarray(im,dtype=np.float32)
-#im_array_gray = rgb2gray(im_array);
+im_array_gray = rgb2gray(im_array_gray);
 if invert_image:
     im_array_gray = (np.abs(255.0-im_array_gray))
 
@@ -55,8 +55,9 @@ im_array_gray_HCT116 = im_array_gray;
 #im = cv2.imread("../Data/Kernels/runawaygrowth_kernels/P1_E3_09.tif",-1); # images of bacterial patch
 im = cv2.imread("../Data/Kernels/HCT116-UnhealthyCell_BacterialPatch.tif");
 im_array_gray = im;# np.asarray(im,dtype=np.float32)
-#im_array_gray = rgb2gray(im_array_gray);
+
 im_array_gray = np.asarray(im,dtype=np.float32)
+im_array_gray = rgb2gray(im_array_gray);
 if True:
     im_array_gray = (np.abs(255.0-im_array_gray))
 im_array_gray_bacterial_patch = im_array_gray/np.max(im_array_gray);
@@ -72,7 +73,7 @@ im = Image.open("../Data/Kernels/HCT116-UnhealthyCell_BacterialPatch.tif");
 #imshow(np.asarray(im))]
 im_array_gray = np.asarray(im,dtype=np.float32)
 #im_array_gray = im;#np.asarray(im,dtype=np.float32)
-#im_array_gray = rgb2gray(im_array_gray);
+im_array_gray = rgb2gray(im_array_gray);
 if invert_image:
      im_array_gray = (np.abs(255.0-im_array_gray))
 

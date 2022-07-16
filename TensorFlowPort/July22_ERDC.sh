9a50f5ad6dbd953763edea29985f9e1993ec1d66
Enoch Yeung <enochyeung@169-231-147-19.wireless.ucsb.edu>
2022-07-15 21:57:10 -0700

    latest updates

--- TensorFlowPort/July22_ERDC.sh
+++ TensorFlowPort/July22_ERDC.sh
@@ -4,7 +4,7 @@
 python3 DynamicFeatureAnalytics.py "/FoFData/20220708_ERDC Handoff/P1_A549ERKFRA1/"  >> 20220708_P1_A549ERKFRA1.txt &
 
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220708_ERDC/P1_HCT116ERKFRA1/"  >> 20220708_P1_HCT116ERKFRA1.txt &
+python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220708_ERDC Handoff/P1_HCT116ERKFRA1/"  >> 20220708_P1_HCT116ERKFRA1.txt &
 
 
 

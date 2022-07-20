9d800d1663e8b4737a8be42b0ed2dd2bc67fe019
Enoch Yeung <enochyeung@Enochs-MacBook-Pro.local>
2022-07-19 23:04:23 -0700

    latest corrections

--- TensorFlowPort/July22_NISTIsolateAnalysis.sh
+++ TensorFlowPort/July22_NISTIsolateAnalysis.sh
@@ -14,9 +14,9 @@ python3 DynamicFeatureAnalytics.py "/FoFData/20220224_NIST Challenge Assay/P1_A5
 
 python3 DynamicFeatureAnalytics.py "/FoFData/20220224_NIST Challenge Assay/P2_A549_ERKFRA1/"  >> 20220224_P2_A549ERKFRA1.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220309_NIST Challenge Assay/P1_HCT116_ERKFRA1/"  >> 20220309_P1_HCT116ERKFRA1.txt &
+python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220309_NIST Challenge Assay/P1_HCT116_ERKFra1/"  >> 20220309_P1_HCT116ERKFRA1.txt &
 
-python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220309_NIST Challenge Assay/P2_HCT116_ERKFRA1/"  >> 20220309_P2_HCT116ERKFRA1.txt &
+python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220309_NIST Challenge Assay/P2_HCT116_ERKFra1/"  >> 20220309_P2_HCT116ERKFRA1.txt &
 
 
 python3 DynamicFeatureAnalytics_HCT116.py "/FoFData/20220303_NIST Challenge Assay/P1_HCT116_ERKFRA1/"  >> 20220303_P1_HCT116ERKFRA1.txt &
